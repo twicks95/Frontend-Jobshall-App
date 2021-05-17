8ba6ef550497d786e380dcaa095a3004fca6a81b
idoy <alfinramadhan97@gmail.com>
2021-05-17 17:36:04 +0700

    test2

--- src/pages/auth/login/Login.js
+++ src/pages/auth/login/Login.js
@@ -5,7 +5,8 @@ class Login extends Component {
   render() {
     return (
       <>
-        <h1 className={styles.mainText}>Halaman Login12321312</h1>
+        <h1 className={styles.mainText}>Halaman Login</h1>
+        <h2>test branch</h2>
       </>
     );
   }
