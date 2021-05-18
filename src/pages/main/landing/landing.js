9ea55dc943f40fd9cce6bf0aa9700cd10d69cb64
twicks95 <teguh.wck95@gmail.com>
2021-05-18 12:02:27 +0700

    Create landing page

--- src/pages/main/landing/landing.js
+++ src/pages/main/landing/landing.js
@@ -1,11 +1,21 @@
 import React, { Component } from "react";
 import styles from "./landing.module.css";
 
+import NavbarComponent from "../../../components/Navbar/Navbar";
+import FooterComponent from "../../../components/Footer/Footer";
+
 class Landing extends Component {
   render() {
     return (
       <>
-        <h1 className={styles.mainText}>Halaman Landing</h1>
+        <NavbarComponent />
+        <section className={`d-flex ${styles.hero}`}>
+          <div>
+            <h1>Talenta terbaik negri untuk perubahan revolusi 4.0</h1>
+          </div>
+          <div></div>
+        </section>
+        <FooterComponent />
       </>
     );
   }
