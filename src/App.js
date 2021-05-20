fe9e2b2e4f933feccf51b33b06d2fa8fa03290c1
twicks95 <teguh.wck95@gmail.com>
2021-05-20 13:05:26 +0700

    Update recruiter profile and App.js

--- src/App.js
+++ src/App.js
@@ -4,17 +4,23 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 
 import Landing from "./pages/main/landing/landing";
-import Register from "./pages/auth/register/Register";
 import Login from "./pages/auth/login/Login";
+import RecruiterProfile from "./pages/main/RecruiterProfile/RecruiterProfile";
+import RecruiterEditProfile from "./pages/main/RecruiterEditProfile/RecruiterEditProfile";
 
 class App extends Component {
   render() {
     return (
       <Router>
         <Switch>
-          <Route path="/" exact component={Register} />
+          <Route path="/" exact component={Landing} />
           <Route path="/login" exact component={Login} />
-          <Route path="/landing" exact component={Landing} />
+          <Route path="/recruiter/profile" exact component={RecruiterProfile} />
+          <Route
+            path="/recruiter/edit"
+            exact
+            component={RecruiterEditProfile}
+          />
         </Switch>
       </Router>
     );
