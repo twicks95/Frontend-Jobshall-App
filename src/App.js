ff8cf1dfc1361389098a1dc05b205212759c1188
twicks95 <teguh.wck95@gmail.com>
2021-05-21 11:26:11 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import "./App.css";
 
+import { Provider } from "react-redux";
+import { store, persistor } from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
+
 import Landing from "./pages/main/landing/landing";
 import Register from "./pages/auth/register/Register";
 import Login from "./pages/auth/login/Login";
@@ -10,13 +14,17 @@ import Login from "./pages/auth/login/Login";
 class App extends Component {
   render() {
     return (
-      <Router>
-        <Switch>
-          <Route path="/" exact component={Register} />
-          <Route path="/login" exact component={Login} />
-          <Route path="/landing" exact component={Landing} />
-        </Switch>
-      </Router>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <Router>
+            <Switch>
+              <Route path="/" exact component={Register} />
+              <Route path="/login" exact component={Login} />
+              <Route path="/landing" exact component={Landing} />
+            </Switch>
+          </Router>
+        </PersistGate>
+      </Provider>
     );
   }
 }
