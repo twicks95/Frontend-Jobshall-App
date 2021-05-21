34a7830a634c73bc32cc1cb6619e54289956d4af
twicks95 <teguh.wck95@gmail.com>
2021-05-21 11:12:43 +0700

    Update navbar component & add more asset

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,30 +1,96 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import { Button, Navbar } from "react-bootstrap";
+import { Button, Dropdown, Navbar } from "react-bootstrap";
 
 import styles from "./Navbar.module.css";
 import logo from "../../assets/images/peworld.png";
 
-const NavbarComponent = (props) => {
-  return (
-    <Navbar
-      bg="light"
-      expand="lg"
-      className={`border border-danger p-0 d-flex justify-content-between ${styles.navbar}`}
-    >
-      <Navbar.Brand href="#home">
-        <Link to="/landing">
-          <img src={logo} alt="logo" className={`${styles.logo}`} />
-        </Link>
-      </Navbar.Brand>
-      <div>
-        <Button variant="outline-primary" className="mr-2">
-          Daftar
-        </Button>
-        <Button variant="primary">Masuk</Button>
-      </div>
-    </Navbar>
-  );
-};
+class NavbarComponent extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoggedIn: true,
+    };
+  }
+
+  renderNav = () => {
+    const { isLoggedIn } = this.state;
+    const { isLanding } = this.props;
+    if (isLanding && isLoggedIn) {
+      return (
+        <div className="d-flex">
+          <Button variant="outline-primary" className="me-3">
+            Masuk
+          </Button>
+          <Dropdown>
+            <Dropdown.Toggle variant="primary" id="dropdown-basic">
+              Daftar
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu>
+              <Dropdown.Item href="#/action-1">Sebagai Tallent</Dropdown.Item>
+              <Dropdown.Item href="#/action-2">Sebagai Recruiter</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </div>
+      );
+    } else if (!isLanding && isLoggedIn) {
+      return (
+        <div className="d-flex">
+          <Button variant="outline-primary" className="me-3">
+            Masuk
+          </Button>
+          <Dropdown>
+            <Dropdown.Toggle variant="primary" id="dropdown-basic">
+              Daftar
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu>
+              <Dropdown.Item href="#/action-1">Sebagai Tallent</Dropdown.Item>
+              <Dropdown.Item href="#/action-2">Sebagai Recruiter</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </div>
+      );
+    } else {
+      return (
+        <div className="d-flex">
+          <Button variant="outline-primary" className="me-3">
+            Masuk
+          </Button>
+          <Dropdown>
+            <Dropdown.Toggle variant="primary" id="dropdown-basic">
+              Daftar
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu>
+              <Dropdown.Item href="#/action-1">Sebagai Tallent</Dropdown.Item>
+              <Dropdown.Item href="#/action-2">Sebagai Recruiter</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </div>
+      );
+    }
+  };
+
+  render() {
+    console.log(this.props);
+    return (
+      <Navbar
+        bg="light"
+        expand="lg"
+        className={`border border-danger p-0 d-flex justify-content-between ${styles.navbar}`}
+      >
+        <Navbar.Brand href="#home">
+          <Link to="/landing">
+            <img src={logo} alt="logo" className={`${styles.logo}`} />
+          </Link>
+        </Navbar.Brand>
+        {this.renderNav()}
+      </Navbar>
+    );
+  }
+}
 
 export default NavbarComponent;
